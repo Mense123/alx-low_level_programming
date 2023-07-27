49469aa034e0af3c86a41a0380eac898e61c8e3c
Mense123 <Ghommyz@gmail.com>
2023-07-27 09:23:35 -0700

    pointers

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,11 +1,12 @@
-trcat - function that concatenates two strings.
+ c/**
+ * _strcat - function that concatenates two strings.
  *
- *  * @dest: pointer to destnation input
- *   * @src: pointer to source input
- *    *
- *     * Return: pointer to resulting string @dest
- *      */
- char *_strcat(char *dest, char *src)
+ * @dest: pointer to destnation input
+ * @src: pointer to source input
+ *
+ * Return: pointer to resulting string @dest
+ */
+char *_strcat(char *dest, char *src)
 {
 		int c, c2;
 
