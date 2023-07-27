bf7457f1fe4ec0f59d3020f61adc020d968ca268
Mense123 <Ghommyz@gmail.com>
2023-07-27 09:44:56 -0700

    /**

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,34 +1,35 @@
-trncpy - A function that copies a string.
+/**
+ * _strncpy - A function that copies a string.
  *
- *  * @dest: pointer to destination input buffer
- *   * @src: pointer to source input buffer
- *    * @n: bytes of @src
- *     *
- *      * Return: @dest
- *       */
- char *_strncpy(char *dest, char *src, int n)
+ * @dest: pointer to destination input buffer
+ * @src: pointer to source input buffer
+ * @n: bytes of @src
+ *
+ * Return: @dest
+ */
+char *_strncpy(char *dest, char *src, int n)
 {
-		int i;
+	int i;
 
-			/**
-			 * 	 * iterate through src array
-			 * 	 	 * where if there is no null byte
-			 * 	 	 	 * among the first n bytes of source
-			 * 	 	 	 	 * the string placed in dest will not be
-			 * 	 	 	 	 	 * null terminated
-			 * 	 	 	 	 	 	 */
-			for (i = 0; i < n && src[i] != '\0'; i++)
-						dest[i] = src[i];
+	/**
+	 * iterate through src array
+	 * where if there is no null byte
+	 * among the first n bytes of source
+	 * the string placed in dest will not be
+	 * null terminated
+	 */
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
 
-				/**
-				 * 	 * if the length of source is less than n
-				 * 	 	 * write additional nullbytes to dest to
-				 * 	 	 	 * ensure that a total of n bytes is written
-				 * 	 	 	 	 */
-				while (i < n)
-						{
-									dest[i] = '\0';
-											i++;
-												}
-					return (dest);
+	/**
+	 * if the length of source is less than n
+	 * write additional nullbytes to dest to
+	 * ensure that a total of n bytes is written
+	 */
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
 }
