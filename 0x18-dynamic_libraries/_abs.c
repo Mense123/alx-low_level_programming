604f009c5b443c9d404bfd07b92c7cd24266b02b
mense123 <ghommyz@gmail.com>
2023-11-13 15:23:19 -0800

    dynamic libraries

--- 0x18-dynamic_libraries/_abs.c
+++ 0x18-dynamic_libraries/_abs.c
@@ -2,7 +2,6 @@
 /**
  * _abs - computes the absolute value of an integer
  * @n: The int to be operated upon
- * author: Innocentsax
  * Return: returns an unsinged int value to calling function
  */
 int _abs(int n)
--- 0x18-dynamic_libraries/_isalpha.c
+++ 0x18-dynamic_libraries/_isalpha.c
@@ -2,7 +2,6 @@
 /**
  * _isalpha - Return 1 if c is a letter. lowercase or uppercase
  * @c: unsigned int value to be compared with ASCII value
- * author: Innocentsax
  * Return: 1 if its alphabet otherwise 0.
  */
 int _isalpha(int c)
